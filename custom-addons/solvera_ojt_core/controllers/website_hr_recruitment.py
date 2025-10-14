adc8d07089aa92aaa9232eada8bfac80d8879116
haris2303 <hrsccf102@gmail.com>
2025-10-14 09:54:35 +0900

    feat: hr applicant

---
+++ custom-addons/solvera_ojt_core/controllers/website_hr_recruitment.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_hr_recruitment.controllers.main import WebsiteHrRecruitment
+
+class CustomWebsiteHrRecruitment(WebsiteHrRecruitment):
+
+    @http.route('/jobs/apply/<model("hr.job"):job>', type='http', auth="public", website=True, sitemap=True)
+    def jobs_apply(self, job, **post):
+        # --- PENGECEKAN LOGIN BARU ---
+        # request.session.uid akan kosong jika user belum login
+        if not request.session.uid:
+            # Jika user belum login, arahkan ke halaman login.
+            # Parameter 'redirect' akan memberitahu Odoo untuk kembali ke halaman ini setelah login berhasil.
+            return request.redirect('/web/login?redirect=%s' % request.httprequest.full_path)
+        # --- AKHIR PENGECEKAN ---
+
+        # Kode di bawah ini hanya akan berjalan jika user sudah login
+        
+        existing_applicants = request.env['hr.applicant'].sudo().search([])
+
+        response = super(CustomWebsiteHrRecruitment, self).jobs_apply(job, **post)
+        
+        if response.status_code == 200 and 'thank_you' in getattr(response, 'template', ''):
+            
+            new_applicant = request.env['hr.applicant'].sudo().search([
+                ('id', 'not in', existing_applicants.ids)
+            ], order='id desc', limit=1)
+            
+            if new_applicant and not new_applicant.partner_id:
+                # Jika user yang login belum punya partner, tautkan sekarang
+                if not request.env.user.partner_id:
+                    request.env.user.partner_id = request.env['res.partner'].sudo().create({'name': request.env.user.name, 'email': request.env.user.login})
+                new_applicant.sudo().write({'partner_id': request.env.user.partner_id.id})
+
+        return response
--- custom-addons/solvera_ojt_core/data/security_data.py
+++ custom-addons/solvera_ojt_core/data/security_data.py
@@ -1,33 +1,33 @@
-from odoo import api, SUPERUSER_ID
+# from odoo import api, SUPERUSER_ID
 
-def create_portal_user_rule(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+# def create_portal_user_rule(cr, registry):
+#     env = api.Environment(cr, SUPERUSER_ID, {})
 
-    try:
-        model_participant_id = env.ref('solvera_ojt_core.model_ojt_participant').id
-        group_portal_id = env.ref('base.group_portal').id
-        rule_xml_id = 'solvera_ojt_core.ojt_participant_portal_user_rule'
+#     try:
+#         model_participant_id = env.ref('solvera_ojt_core.model_ojt_participant').id
+#         group_portal_id = env.ref('base.group_portal').id
+#         rule_xml_id = 'solvera_ojt_core.ojt_participant_portal_user_rule'
 
-        rule_vals = {
-            'name': 'OJT Participant: Portal User Rule',
-            'model_id': model_participant_id,
-            'groups': [(6, 0, [group_portal_id])],
-            'perm_read': True,
-            'perm_write': False,
-            'perm_create': False,
-            'perm_delete': False,
-            'domain_force': "[('partner_id', '=', user.partner_id.id)]"
-        }
+#         rule_vals = {
+#             'name': 'OJT Participant: Portal User Rule',
+#             'model_id': model_participant_id,
+#             'groups': [(6, 0, [group_portal_id])],
+#             'perm_read': True,
+#             'perm_write': False,
+#             'perm_create': False,
+#             'perm_delete': False,
+#             'domain_force': "[('partner_id', '=', user.partner_id.id)]"
+#         }
 
-        rule = env.ref(rule_xml_id, raise_if_not_found=False)
-        if rule:
-            rule.write(rule_vals)
-        else:
-            # Odoo 16+ requires the module name in the id when creating through code
-            env['ir.rule'].create({
-                'id': rule_xml_id,
-                **rule_vals
-            })
-    except Exception:
-        # Fails silently if refs don't exist yet, will be re-attempted on next update.
-        pass
+#         rule = env.ref(rule_xml_id, raise_if_not_found=False)
+#         if rule:
+#             rule.write(rule_vals)
+#         else:
+#             # Odoo 16+ requires the module name in the id when creating through code
+#             env['ir.rule'].create({
+#                 'id': rule_xml_id,
+#                 **rule_vals
+#             })
+#     except Exception:
+#         # Fails silently if refs don't exist yet, will be re-attempted on next update.
+#         pass
